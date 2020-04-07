f041ceb2f1ecd798e8385aefe6a3dfa2344542a8
Scott Delbango <scott.delbango@gmail.com>
2020-04-07 10:29:00 -0400

    Full pseudocode breakdown. I think.

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log('Working')
+const BASE_URL = `https://breakingbadapi.com/api/`
+
+//Full Pseudocode Instructions
+//1 - Write event listener for start game
+//2 - Write function for event listener that builds the 
+//API request for the first quote.
+//3 - Write function that pulls correct answer from characters
+//4 - Write function that pulls random wrong answers from characters
+//Be sure to exlcude correct answer
+//5 - Write callback function that activates all 3 functions above and displays to DOM
+//6 - Write event listener for selecting answer
+//7 - Write a function that checks for correctness of answer
+//8 - Write function that increments score or removes lives in correct / uncorrect scenarios
+//9 - Write a function that checks for win state
+//10 - Write a function that transitions to the next question
+//11 - Write a function that transitions to win / loss states
+//12 - Write a callback function for answer selector at 6 that passes 7-11 in
+//13 - Write async function to pull data
+//14 - Write play again / reset function
+//15 - Write duplicate prevention into quote function at 2
+
+
