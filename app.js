a9dd2369c6eaab74332bc8b67eecb268cdbea439
Scott Delbango <scott.delbango@gmail.com>
2020-04-07 22:05:26 -0400

    Got the quotes stored and matching author attribute to character

--- app.js
+++ app.js
@@ -5,35 +5,63 @@ const WRONG_ANSWER_URL = `character/random`;
 let CORRECT_URL = ``;
 
 //Full Pseudocode Instructions
-//1 - Write event listener for start game button (Add button w/ class 'start' to HTML)
+//Write event listener for start game button (Add button w/ class 'start' to HTML)
 document.querySelector('button').addEventListener('click', onClick);
-//2 - Write function for event listener that builds the 
+//Write function for event listener that builds the 
 //API request for the first quote.
 function onClick(e) {
   e.preventDefault();
   fetchQuote();
   // fetchCorrectAnswer();
 }
-//3 - Write function that pulls correct answer from characters
+//Write function that pulls correct answer from characters
 
-//4 - Write function that pulls random wrong answers from characters
-
+//Write function that pulls random wrong answers from characters
 //Be sure to exlcude correct answer
-//5 - Write callback function that activates all 3 functions above and displays to DOM
-//6 - Write event listener for selecting answers
-//7 - Write a function that checks for correctness of answer
-//8 - Write function that increments score or removes lives in correct / uncorrect scenarios
-//9 - Write a function that checks for win state
-//10 - Write a function that transitions to the next question (based on start game function - can this be DRY?)
-//11 - Write a function that transitions to win / loss states
-//12 - Write a callback function for answer selector at 6 that passes 7-11 in
-//13 - Write async function to pull data
-//14 - Write function to randomize position of answers
-//15 - Write function that changes start button to restart button
+
+//Write callback function that displays quote to DOM & separate function that displays answers
+
+//Write event listener for selecting answers
+
+//Write a function that checks for correctness of answer
+
+//Write function that increments score or removes lives in correct / incorrect scenarios
+
+//Write a function that checks for win state
+
+//Write a function that transitions to the next question (based on start game function - can this be DRY?)
+
+// Write a function that transitions to win / loss states
+
+//Write a callback function for answer selector at 6 that passes in 7-11
+
+//Write async function to pull data
+
+//Write function to randomize position of answers
+
+//Write function that changes start button to restart button
+
+//
+function accessData(response) {
+  //create variable for the quote
+  let quote = response.data;
+  console.log(quote[0].quote)
+  CORRECT_URL = `characters?=${quote[0].author}`
+  if (CORRECT_URL.includes(" ")) {
+    CORRECT_URL = CORRECT_URL.replace(/\s/g, "+"); //Shout to https://flaviocopes.com/how-to-replace-whitespace-javascript/ for the regEx help
+  }
+  console.log(CORRECT_URL);
+
+  // ?name = Walter + White
+  // //select movie list
+  // const selectMovieList = document.querySelector(".movie-list");
+  // //iterate through the results
+}
+
+
 async function fetchQuote() {
   const response = await axios.get(BASE_URL + QUOTE_URL);
-  console.log(response)
-  // displayData(response);
+  accessData(response);
 };
 
 // async function fetchCorrectAnswer() {
