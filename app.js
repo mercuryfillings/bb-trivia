b843bcfbfd7a3b509708351e55f627aa204e0ea4
Scott Delbango <scott.delbango@gmail.com>
2020-04-10 10:47:57 -0400

    Cleaned up code and added comments

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ let currentAnswers = [];
 
 document.querySelector('.start').addEventListener('click', onClick);
 
+
+//Start game on click
 function onClick(e) {
   e.preventDefault();
   let button = document.querySelector('button');
@@ -22,8 +24,10 @@ function onClick(e) {
   }
 }
 
+//Access data pulled from fetchQuote & handle unique cases, 
+//then fire display, and fire access wrong answer 1 & 2
+
 function accessQuote(response) {
-  //create variable for the quote
   let quote = response.data;
   console.log(quote[0].quote)
   correctAnswer = quote[0].author;
@@ -55,11 +59,13 @@ function accessQuote(response) {
   fetchWrongAnswer2();
 }
 
+//Display the quote from accessQuote above
 function displayQuote(quote) {
   const quoteText = document.querySelector('.quote');
   quoteText.innerHTML = `\"${quote[0].quote}\"`;
 }
 
+//Access data from the correct character fetch, check for duplicates
 function accessCorrect(response) {
   let correctCharacterData = response.data;
   let record = correctCharacterData[0].name;
@@ -73,9 +79,10 @@ function accessCorrect(response) {
   currentAnswers.push(record);
   console.log(currentAnswers);
   displayCorrect(correctCharacterData);
-  // let correctName = correctCharacter[0].name; //Figure out how to overlay in CSS post MVP
+  // let correctName = correctCharacter[0].name; //Add overlay with name in CSS post MVP
 }
 
+//Display image of correct answer above to DOM
 function displayCorrect(correctCharacterData) {
   let li = document.createElement('li');
   li.className = 'circle';
@@ -89,6 +96,7 @@ function displayCorrect(correctCharacterData) {
   applyAnswerListener();
 }
 
+//Access wrong answer from fetchWrongAnswer1 async function below & check for duplicates
 function accessIncorrect1(response) {
   let incorrectCharacterData = response.data;
   let record = incorrectCharacterData[0].name;
@@ -104,8 +112,8 @@ function accessIncorrect1(response) {
   // let correctName = correctCharacter[0].name; //Figure out how to overlay in CSS post MVP
 }
 
+//Display image from accessIncorrect1 to DOM
 function displayIncorrect1(incorrectCharacterData) {
-  let record = incorrectCharacterData[0].name;
   let li = document.createElement('li');
   li.className = 'circle';
   let img = document.createElement('img');
@@ -116,6 +124,7 @@ function displayIncorrect1(incorrectCharacterData) {
   incorrectContent.appendChild(li);
 }
 
+//Access a second incorrect answer from fetchWrongAnswer2 async function below & check for duplicates
 function accessIncorrect2(response) {
   let incorrectCharacterData = response.data;
   let record = (incorrectCharacterData[0].name);
@@ -131,8 +140,8 @@ function accessIncorrect2(response) {
   // let correctName = correctCharacter[0].name; //Figure out how to overlay in CSS post MVP
 }
 
+//Display 2nd incorrect image to DOM
 function displayIncorrect2(incorrectCharacterData) {
-  let record = (incorrectCharacterData[0].name);
   let li = document.createElement('li');
   li.className = 'circle';
   let img = document.createElement('img');
@@ -143,27 +152,65 @@ function displayIncorrect2(incorrectCharacterData) {
   incorrectContent.appendChild(li);
 }
 
+//Dedup still a work in progress - for now, janky quote reset solution in place
+function accessDedup() {
+  let dedupData = response.data;
+  let record = (dedupData[0].name);
+  if (currentAnswers.includes(record)) {
+    clearDuplicate();
+    fetchDedup();
+    console.log("Dedup")
+    return;
+  }
+  currentAnswers.push(record);
+  displayDedup(incorrectCharacterData);
+}
+
+//Unused dedupe improvements. DO NOT LOOK, I'M NOT DECENT.
+function displayDedup(newCharacterData) {
+  let record = (newCharacterData[0].name);
+  let li = document.createElement('li');
+  li.className = 'circle';
+  let img = document.createElement('img');
+  img.title = `${newCharacterData[0].name}`;
+  img.src = `${newCharacterData[0].img}`;
+  li.appendChild(img);
+  const replacementContent = document.querySelector('.answerbox');
+  replacementContent.appendChild(li);
+}
+
+//Pulls random quote from API through built-in random functionality
 async function fetchQuote() {
   const response = await axios.get(BASE_URL + QUOTE_URL);
   accessQuote(response);
 };
 
+//Pulls character data for the character who spoke the quote fetched in the preceding function
 async function fetchCorrectAnswer() {
   const response = await axios.get(BASE_URL + CORRECT_URL);
   accessCorrect(response);
 };
 
+//Fetches an incorrect answer
 async function fetchWrongAnswer1() {
   const response = await axios.get(BASE_URL + WRONG_ANSWER_URL);
   accessIncorrect1(response);
 };
 
+//Fetches a second incorrect answer. These didn't seem to work in parallel, so I broke it into 2.
 async function fetchWrongAnswer2() {
   const response = await axios.get(BASE_URL + WRONG_ANSWER_URL);
   accessIncorrect2(response);
   fetchCorrectAnswer();
 };
 
+//Unused dedupe improvements. DO NOT LOOK, I'M NOT DECENT.
+async function fetchDedup() {
+  const response = await axios.get(BASE_URL + WRONG_ANSWER_URL);
+  accessDedup(response);
+}
+
+//Starts the round, clears currentAnswer tracking array, and clears the board if anything exists
 function playRound() {
   if (document.querySelector('.answerbox').hasChildNodes()) {
     clearBoard()
@@ -172,11 +219,13 @@ function playRound() {
   fetchQuote();
 }
 
+//Clears the board area
 function clearBoard() {
   const parent = document.querySelector('.answerbox');
   parent.querySelectorAll('*').forEach(n => n.remove())
 }
 
+//Clears the board and hud from the DOM
 function clearAll() {
   const quote = document.querySelector('.quotebox');
   quote.remove();
@@ -185,6 +234,12 @@ function clearAll() {
   clearBoard();
 }
 
+//Unused dedupe improvements. DO NOT LOOK, I'M NOT DECENT.
+function clearDuplicate() {
+  let dupe = document.querySelector()
+} //TODO
+
+//Applies click functionality to all answer choices
 function applyAnswerListener() {
   document.querySelectorAll('.circle').forEach((circle) => {
     circle.addEventListener('click', answerClick);
@@ -192,9 +247,10 @@ function applyAnswerListener() {
   shuffle();
 }
 
+//Shows if a chosen answer is correct or incorrect, increments score or removes a life accordingly, 
+//and checks for win / loss state
 function answerClick(e) {
   e.preventDefault();
-  currentAnswers = [];
   let targetElement = event.target;
   let lives = document.querySelector('.lives');
   let score = document.querySelector('.score').textContent;
@@ -217,6 +273,7 @@ function answerClick(e) {
   }
 }
 
+//Checks conditions for win loss, delivers win loss message if met, and starts next round if not.
 function checkWinLoss() {
   let lives = document.querySelector('.lives');
   let score = document.querySelector('.score').textContent;
@@ -234,6 +291,7 @@ function checkWinLoss() {
   } else setTimeout(playRound, 900);
 }
 
+//Shuffles answers so correct isn't always in the same spot
 function shuffle() {
   let rando = Math.floor(Math.random() * Math.floor(5));
   for (let i = 1; i <= rando; i++) {
